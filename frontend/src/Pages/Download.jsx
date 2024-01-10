7b29f7717c24f01842662c27a4ba9c185bfdb774
anannyaSaikia <anannyasaikia1998@gmail.com>
2024-01-10 14:55:21 +0530

    change in data type

--- frontend/src/Pages/Download.jsx
+++ frontend/src/Pages/Download.jsx
@@ -4,25 +4,25 @@ import { saveAs } from 'file-saver';
 import style from '../Styles/Download.module.css';
 
 const Download = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState({})
 
     const token = localStorage.getItem("token")
 
-    const handleDownload = () =>{
+    const handleDownload = () => {
         axios.get("https://kryzen-project-backend.cyclic.app/form/download", {
             headers: {
                 "Authorization": `Bearer ${token}`
             },
-            responseType : 'blob'
+            responseType: 'blob'
         })
-        .then((res)=>{
-            //console.log("blob data : " + res.data)
-            const pdfBlob = new Blob([res.data], {type : 'application/pdf'})
-            saveAs(pdfBlob, 'user.pdf')
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
+            .then((res) => {
+                //console.log("blob data : " + res.data)
+                const pdfBlob = new Blob([res.data], { type: 'application/pdf' })
+                saveAs(pdfBlob, 'user.pdf')
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 
     const getData = () => {
@@ -33,7 +33,7 @@ const Download = () => {
         })
             .then((res) => {
                 console.log(res.data)
-                setData(res.data.data)
+                setData(res.data.data[0])
             })
             .catch((err) => {
                 console.log(err)
@@ -48,7 +48,7 @@ const Download = () => {
         <div className={style.main}>
             <h3>Your Details</h3>
             <div>
-                {
+                {/* {
                     data?.map((ele, i) => {
                         return <div key={i}>
                             <img src={`https://kryzen-project-backend.cyclic.app/uploads/${ele.image}`} alt="avatar" />
@@ -57,6 +57,15 @@ const Download = () => {
                             <p>Address : {ele.address}</p>
                         </div>
                     })
+                } */}
+                {
+                    data ? <div>
+                        <img src={`https://kryzen-project-backend.cyclic.app/uploads/${data.image}`} alt="avatar" />
+                        <p>Name : {data.name}</p>
+                        <p>Age : {data.age}</p>
+                        <p>Address : {data.address}</p>
+                    </div>
+                        : <h4>No Data Found</h4>
                 }
             </div>
             <button onClick={handleDownload}>Download</button>
